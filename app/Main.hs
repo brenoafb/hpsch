46d73888f2605b6262f641a5436d3cdc387f7905
brenoafb <breno.afb@gmail.com>
2020-11-10 18:57:30 -0300

    Modularize UI components

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main where
 
 import Lib
+import UI
 
 import qualified Data.List as L
 import qualified Data.Text as T
@@ -10,14 +12,15 @@ import qualified Data.Time.Calendar as Cal
 import qualified Data.Time.Clock as Cl
 import qualified System.Console.Haskeline as Hl
 import qualified Data.ByteString.Lazy as B
+import qualified Data.Map as M
 
 import Text.Read (readMaybe)
 import Control.Monad
 import Control.Monad.Trans
+import Control.Monad.State
+import Control.Monad.Identity
 import Data.Aeson
 import Data.Aeson.Encode.Pretty
-
-type Repl a = Hl.InputT IO a
 
 main :: IO ()
 main = do
@@ -28,67 +31,11 @@ main = do
       today <- getDay
       let (todaysTasks, remainingTasks) = L.partition (\task -> date task <= today) tasks
       putStrLn $ show (length todaysTasks) ++ " tasks allocated for today"
-      putStrLn commandLine
-      (done, delegated) <- Hl.runInputT Hl.defaultSettings (loop todaysTasks)
+      -- putStrLn commandLine
+      -- (done, delegated, _) <- Hl.runInputT Hl.defaultSettings $ execStateT (runRepl loop) ([], [], todaysTasks)
+      let taskState = TaskState [] [] todaysTasks
+      taskState' <- Hl.runInputT Hl.defaultSettings $ execStateT loop taskState
       putStrLn "Done!"
-      let tasks' = remainingTasks ++ done ++ delegated
+      let tasks' = remainingTasks ++ done taskState' ++ delegated taskState'
       B.writeFile "tasklist" $ encodePretty tasks'
       return ()
-
-
-loop :: [Task] -> Repl ([Task], [Task])
-loop = go [] []
-  where go :: [Task] -> [Task] -> [Task] -> Repl ([Task], [Task])
-        go done delegated [] = return (done, delegated)
-        go done delegated (task:tasks) = do
-          Hl.outputStr $ "\n" ++ show task ++ "\n"
-          c <- Hl.getInputChar "> "
-          today <- liftIO getDay
-          case c of
-
-            Just 'd' -> do
-              newInfo <- (T.pack <$>) <$> Hl.getInputLine "New info: "  -- do task and schedule for later with add new description
-              let task' = doTask today newInfo task
-              go (task' <:> done) delegated tasks
-
-            Just 'D' -> go (doTask today Nothing task <:> done) delegated tasks -- do task and schedule for later (maintain current description)
-
-            Just 'x' -> go done (task:delegated) tasks                      -- delegate task for tomorrow
-
-            Just 'l' -> go done delegated $ tasks ++ [task]                 -- push task to end of queue
-
-            Just 'i' -> do                                                  -- adjust interval
-              input <- Hl.getInputLine "New interval: "
-              let nim = input >>= readMaybe
-              case nim of
-                Nothing -> Hl.outputStr "Invalid interval" >> go done delegated (task:tasks)
-                Just ni -> let task' = task { interval = ni }
-                  in go done delegated (task':tasks)
-
-            Just '?' -> Hl.outputStr commandLine >> go done delegated (task:tasks)
-
-            Just 'q' -> return (done, delegated ++ (task:tasks))
-
-            Nothing -> return (done, delegated ++ (task:tasks))
-
-            _   -> go done delegated (task:tasks)
-
-commandLine =
-  "\nd: Finish task and write new description\n\
-   \D: finish task and maintain current description\n\
-   \x: set task aside\n\
-   \l: push task to end of queue\n\
-   \i: adjust current interval\n\
-   \?: show commands\n\
-   \q: quit (pending tasks are delegated)"
-
-(<:>) :: Maybe a -> [a] -> [a]
-Nothing <:> xs = xs
-Just x <:> xs = x:xs
-
-printList :: Show a => [a] -> IO ()
-printList xs =
-  mapM_ putStr $ L.intersperse "\n\n" (map show xs) ++ ["\n"]
-
-getDay :: IO Cal.Day
-getDay = Cl.utctDay <$> Cl.getCurrentTime
---
+++ src/UI.hs
@@ -0,0 +1,156 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module UI where
+
+import Lib
+
+import qualified Data.Text as T
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Time.Calendar as Cal
+import qualified Data.Time.Clock as Cl
+import qualified System.Console.Haskeline as Hl
+import qualified Data.ByteString.Lazy as B
+
+import Text.Read (readMaybe)
+
+import Control.Monad.State
+
+data TaskState = TaskState
+  { done :: [Task]
+  , delegated :: [Task]
+  , current :: [Task]
+  } deriving (Eq, Show)
+
+data Action = Action
+  { key :: Char
+  , description :: T.Text
+  , handler :: Repl () -> Repl ()
+  }
+
+instance Show Action where
+  show action = key action : "\t" ++ d
+    where d = T.unpack $ description action
+
+type Repl a = StateT TaskState (Hl.InputT IO) a
+
+loop :: Repl ()
+loop = do
+  tasks <- gets current
+  if null tasks
+     then return ()
+     else do
+       let task = head tasks
+       lift (Hl.outputStr $ "\n" ++ show task ++ "\n")
+       cm <- lift $ Hl.getInputChar "> "
+       case cm of
+         Nothing -> loop
+         Just c -> case M.lookup c actions of
+           Nothing -> loop
+           Just action -> handler action loop
+
+commandLine :: T.Text
+commandLine = T.unlines $ map (\(k, action) -> T.pack $ show action) $ M.toList actions
+
+
+buildActionMap :: [Action] -> M.Map Char Action
+buildActionMap = M.fromList . map (\action -> (key action, action))
+
+(<:>) :: Maybe a -> [a] -> [a]
+Nothing <:> xs = xs
+Just x <:> xs = x:xs
+
+printList :: Show a => [a] -> IO ()
+printList xs =
+  mapM_ putStr $ L.intersperse "\n\n" (map show xs) ++ ["\n"]
+
+getDay :: IO Cal.Day
+getDay = Cl.utctDay <$> Cl.getCurrentTime
+
+actions :: M.Map Char Action
+actions = buildActionMap
+  [ Action
+      { key = 'd'
+      , description = "Complete task and edit task description"
+      , handler = \cont -> do
+          tasks' <- gets current
+          if null tasks' then cont
+            else do
+              let (task:tasks) = tasks'
+              newInfo <- lift $ (T.pack <$>) <$> Hl.getInputLine "New info: "
+              today <- liftIO getDay
+              let task' = doTask today newInfo task
+              modify $ \taskState -> taskState { done = task' <:> done taskState, current = tasks }
+              cont
+      }
+  , Action
+     { key = 'D'
+      , description = "Complete current task"
+      , handler = \cont -> do
+          tasks' <- gets current
+          if null tasks' then cont
+          else do
+            let (task:tasks) = tasks'
+            today <- liftIO getDay
+            let task' = doTask today Nothing task
+            modify $ \taskState -> taskState { done = task' <:> done taskState, current = tasks }
+            cont
+     }
+  , Action
+      { key = 'x'
+      , description = "Delegate task for tomorrow"
+      , handler = \cont -> do
+          tasks' <- gets current
+          if null tasks' then cont
+            else do
+              let (task:tasks) = tasks'
+              taskState <- get
+              put $ taskState { delegated = task : delegated taskState , current = tasks}
+              cont
+
+      }
+  , Action
+      { key = 'l'
+      , description = "Push current task to the end of the queue"
+      , handler = \cont -> do
+          tasks' <- gets current
+          if null tasks' then cont
+            else do
+              let (task:tasks) = tasks'
+              taskState <- get
+              put $ taskState { current = tasks ++ [task] }
+              cont
+
+      }
+  , Action
+      { key = 'i'
+      , description = "Adjust current task's interval"
+      , handler = \cont -> do
+          tasks' <- gets current
+          if null tasks' then cont
+            else do
+              let (task:tasks) = tasks'
+              input <- lift $ Hl.getInputLine "New interval: "
+              let nim = input >>= readMaybe
+              case nim of
+                Nothing -> lift (Hl.outputStr "Invalid interval") >> cont
+                Just ni -> do
+                  let task' = task { interval = ni }
+                  modify $ \taskState -> taskState { current = task':tasks }
+                  cont
+
+      }
+  , Action
+      { key = '?'
+      , description = "Show commands"
+      , handler = \cont -> do
+          lift $ Hl.outputStr $ T.unpack commandLine
+          cont
+      }
+  , Action
+      { key = 'q'
+      , description = "Quit (current and queued tasks are delegated)"
+      , handler = \cont ->
+          modify $ \taskState -> taskState { delegated = current taskState, current = [] }
+      }
+  ]
